52fad8edf4a089f15b601fc49a379a1ba004f795
Robert Andersson <robban2005@gmail.com>
2022-04-05 02:15:57 +0700

    mysql

--- app/packages/server/src/routes.ts
+++ app/packages/server/src/routes.ts
@@ -1,11 +1,22 @@
+import { Sequelize } from 'sequelize';
 import { Router } from 'express';
-import {RobertLearnsTest} from "@elr0berto/robert-learns-shared/src/test";
+//import {RobertLearnsTest} from "@elr0berto/robert-learns-shared/src/test";
+
+
+const sequelize = new Sequelize('robertlearns', 'robertlearns', 'robertlearns', {
+    host: 'localhost',
+    dialect: 'mysql',
+});
 
 const routes = Router();
 
-routes.get('/api', (_, res) => {
-    const x : RobertLearnsTest = {xxx:true};
-    return res.json({ message: 'Hello World API' });
+routes.get('/api', async (_, res) => {
+    try {
+        await sequelize.authenticate();
+        return res.json({message: 'db Succuess'});
+    } catch (error) {
+        return res.json({message: 'db suxx'});
+    }
 });
 
 export default routes;
