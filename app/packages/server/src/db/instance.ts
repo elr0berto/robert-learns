505ddfa046fb00e28e92341f5122ecd62b1f4a91
Robert Andersson <robban2005@gmail.com>
2022-04-08 23:21:16 +0700

    db stuff

--- app/packages/server/src/db/instance.ts
+++
@@ -1,8 +0,0 @@
-import {Sequelize} from "sequelize";
-
-const sequelize = new Sequelize('robertlearns', 'robertlearns', 'robertlearns', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-
-export default sequelize;
--- app/packages/server/src/routes.ts
+++ app/packages/server/src/routes.ts
@@ -1,6 +1,6 @@
-import { Sequelize } from 'sequelize';
 import { Router } from 'express';
-import sequelize from "./db/instance";
+import { PrismaClient } from '@prisma/client';
+import prisma from "./db/prisma";
 //import {RobertLearnsTest} from "@elr0berto/robert-learns-shared/src/test";
 
 
@@ -8,12 +8,20 @@ import sequelize from "./db/instance";
 const routes = Router();
 
 routes.get('/api', async (_, res) => {
-    try {
-        await sequelize.authenticate();
-        return res.json({message: 'db Succuess'});
-    } catch (error) {
-        return res.json({message: 'db suxx'});
-    }
+    await prisma.user.create({
+        data: {
+            firstName: 'Alice',
+            lastName: 'Wonder',
+            password: 'abc123',
+            email: 'aliddce@prisma.io',
+        },
+    })
+
+
+    const allUsers = await prisma.user.findMany()
+    console.log('XXX:',allUsers)
+
+    return res.json({message: 'db Succuess'});
 });
 
 export default routes;
